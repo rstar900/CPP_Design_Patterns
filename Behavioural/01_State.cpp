f007fcdd351bbe03958e2b0703479f58c87be3e8
Rachit Garg <rachitgarg900@gmail.com>
2024-03-03 21:15:31 +0100

    Completed basic code without some optimizations

--- Behavioural/01_State.cpp
+++ Behavioural/01_State.cpp
@@ -12,16 +12,20 @@
 // And https://refactoring.guru/design-patterns/state/cpp/example
 // And https://doc.rust-lang.org/book/ch17-03-oo-design-patterns.html
 
+// TODO: Optimizations for setContext() and bring the Published State methods out for uniformity
+
 #include <iostream>
+
+// Forward declaration of Post class, so that PostState recognizes it
+class Post;
 
 // The State interface
 class PostState
 {
 
-    // To allow post objects to change the internal context
+    // Private Context pointer will be in concrete States
+    // Also declare Post to be a friend class in concrete States as well
     friend class Post;
-
-    // Private Context pointer will be in concrete States 
 
 protected:
     // function to set Context
@@ -38,122 +42,13 @@ public:
     
 };
 
-// The State classes
-
-class Draft final: public PostState
-{
-    // Private Context pointer
-    Post* m_Post{};
-
-    // overrides
-protected:
-    void setContext(Post* post)
-    {
-        m_Post = post;
-    }
-    virtual void viewContent() override
-    {
-        std::cout << "[Draft State:] Cannot view post yet." << std::endl;
-    }
-    virtual void addContent(std::string& content) override
-    {
-        std::cout << "[Draft State:] Added content, changing to InReview state..." << std::endl;
-        m_post->m_content.append(content);
-        m_post->changeState(new InReview());
-    }
-    virtual void reviewContent(bool isPassing) override
-    {
-        std::cout << "[Draft State:] Cannot review post yet." << std::endl;
-    }
-
-public:
-    // Contructor
-    Draft()
-    {
-        "[Draft State:] Welcome!" << std::endl;
-    }
-    
-};
-
-class InReview final: public PostState
-{
-    // Private Context pointer
-    Post* m_Post{};
-
-    // overrides
-protected:
-    void setContext(Post* post)
-    {
-        m_Post = post;
-    }
-    virtual void viewContent() override
-    {
-        std::cout << "[InReview State:] Cannot view post yet." << std::endl;
-    }
-    virtual void addContent(std::string& content) override
-    {
-        std::cout << "[InReview State:] Cannot edit post unless in Draft state." << std::endl;
-    }
-    virtual void reviewContent(bool isPassing) override
-    {
-        if (isPassing) 
-        {
-            std::cout << "[InReview State:] Review successful, changing to Published state..." << std::endl;
-            m_post->changeState(new Published());
-        }
-        else
-        {
-            std::cout << "[InReview State:] Review unsuccessful, changing back to draft state..." << std::endl;
-            m_post->changeState(new Draft());
-        }    
-    }
-
-public:
-    // Contructor
-    InReview()
-    {
-        "[InReview State:] Welcome!" << std::endl;
-    }
-    
-};
-
-class Published final: public PostState
-{
-    // Private Context pointer
-    Post* m_Post{};
-
-    // overrides
-protected:
-    void setContext(Post* post)
-    {
-        m_Post = post;
-    }
-    virtual void viewContent() override
-    {
-        std::cout << "[Published State:] " << m_post->m_content << std::endl;
-    }
-    virtual void addContent(std::string& content) override
-    {
-        std::cout << "[Published State:] Cannot edit post unless in Draft state." << std::endl;
-    }
-    virtual void reviewContent(bool isPassing) override
-    {
-        std::cout << "[Published State:] Cannot review post after publishing." << std::endl;
-    }
-
-public:
-    // Contructor
-    Published()
-    {
-        "[Published State:] Welcome!" << std::endl;
-    }
-};
-
 // The Context class
 class Post 
 {
     // To allow state objects to change internal state of the post object
-    friend class PostState;
+    friend class Draft;
+    friend class InReview;
+    friend class Published;
 
     // The private State object pointer
     PostState* m_postState{};
@@ -173,29 +68,15 @@ class Post
     }
 
 public:
-    Post() 
-    {
-        // Set initial state to be Draft
-        m_postState = new Draft();
-        m_postState->setContext(this);
-    }
+    Post();
 
     // State dependent functions
     // We delegate the state based functionality to the respective state objects
-    void viewContent()
-    {
-        m_postState->viewContent();
-    }
+    void viewContent();
 
-    void addContent(std::string& content)
-    {
-        m_postState->addContent(content);
-    }
+    void addContent(std::string& content);
 
-    void reviewContent(bool isPassing)
-    {
-        m_postState->reviewContent(isPassing);
-    }
+    void reviewContent(bool isPassing);
 
     // Destructor
     ~Post()
@@ -204,3 +85,194 @@ public:
     }
 
 };
+
+
+// The State classes
+
+class Draft final: public PostState
+{
+    // To allow post objects to change the internal context
+    friend class Post;
+
+    // Private Context pointer
+    Post* m_post{};
+
+    // overrides
+protected:
+    void setContext(Post* post);
+    virtual void viewContent() override;
+    virtual void addContent(std::string& content) override;
+    virtual void reviewContent(bool isPassing) override;
+
+public:
+    // Contructor
+    Draft()
+    {
+       std::cout << "[Draft State:] Welcome!" << std::endl;
+    }
+    
+};
+
+class InReview final: public PostState
+{
+    // To allow post objects to change the internal context
+    friend class Post;
+
+    // Private Context pointer
+    Post* m_post{};
+
+    // overrides
+protected:
+    void setContext(Post* post);
+    virtual void viewContent() override;
+    virtual void addContent(std::string& content) override;
+    virtual void reviewContent(bool isPassing) override;
+
+public:
+    // Contructor
+    InReview()
+    {
+       std::cout << "[InReview State:] Welcome!" << std::endl;
+    }
+    
+};
+
+class Published final: public PostState
+{
+    // To allow post objects to change the internal context
+    friend class Post;
+
+    // Private Context pointer
+    Post* m_post{};
+
+    // overrides
+protected:
+    void setContext(Post* post)
+    {
+        m_post = post;
+    }
+    virtual void viewContent() override
+    {
+        std::cout << "[Published State:] " << m_post->m_content << std::endl;
+    }
+    virtual void addContent(std::string& content) override
+    {
+        std::cout << "[Published State:] Cannot edit post unless in Draft state." << std::endl;
+    }
+    virtual void reviewContent(bool isPassing) override
+    {
+        std::cout << "[Published State:] Cannot review post after publishing." << std::endl;
+    }
+
+public:
+    // Contructor
+    Published()
+    {
+       std::cout << "[Published State:] Welcome!" << std::endl;
+    }
+};
+
+// ---- Post public function Implementations starts ----
+
+Post::Post() 
+{
+    // Set initial state to be Draft
+    m_postState = new Draft();
+    m_postState->setContext(this);
+}
+
+void Post::viewContent()
+{
+    m_postState->viewContent();
+}
+
+void Post::addContent(std::string& content)
+{
+    m_postState->addContent(content);
+}
+
+void Post::reviewContent(bool isPassing)
+{
+    m_postState->reviewContent(isPassing);
+}
+
+// ---- Post public function Implementations Ends ----
+
+
+// ---- Draft function implementations Start ----
+
+void Draft::setContext(Post* post)
+{
+    m_post = post;
+}
+
+void Draft::viewContent()
+{
+    std::cout << "[Draft State:] Cannot view post yet." << std::endl;
+}
+
+void Draft::addContent(std::string& content)
+{
+    std::cout << "[Draft State:] Added content, changing to InReview state..." << std::endl;
+    m_post->m_content.append(content);
+    m_post->changeState(new InReview());
+}
+
+void Draft::reviewContent(bool isPassing)
+{
+    std::cout << "[Draft State:] Cannot review post yet." << std::endl;
+}
+
+// ---- Draft function implementations End ----
+
+
+// ---- InReview function implementations Start ----
+
+void InReview::setContext(Post* post)
+{
+    m_post = post;
+}
+
+void InReview::viewContent()
+{
+    std::cout << "[InReview State:] Cannot view post yet." << std::endl;
+}
+
+void InReview::addContent(std::string& content)
+{
+    std::cout << "[InReview State:] Cannot edit post unless in Draft state." << std::endl;
+}
+ 
+void InReview::reviewContent(bool isPassing)
+{
+    if (isPassing) 
+    {
+        std::cout << "[InReview State:] Review successful, changing to Published state..." << std::endl;
+        m_post->changeState(new Published());
+    }
+    else
+    {
+        std::cout << "[InReview State:] Review unsuccessful, changing back to draft state..." << std::endl;
+        m_post->changeState(new Draft());
+    }    
+}
+
+// ---- InReview function implementations End ----
+
+int main()
+{
+    Post post;
+    std::string content{"Hello from published post"};
+    
+    post.addContent(content);
+    post.viewContent();
+
+    post.reviewContent(false);
+
+    std::string content_addition{"\nSome more additions"};
+    post.addContent(content_addition);
+    post.viewContent();
+
+    post.reviewContent(true);
+    post.viewContent();
+}
